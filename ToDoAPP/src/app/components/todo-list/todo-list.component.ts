f6dd8f92208c3c9a3dd9903c7ecde97430aa5a2b
SantiagoLoaiza247 <128566879+darkfast247@users.noreply.github.com>
2024-04-18 13:05:15 -0500

    segundo commit

--- ToDoAPP/src/app/components/todo-list/todo-list.component.ts
+++ ToDoAPP/src/app/components/todo-list/todo-list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+// todo-list.component.ts
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/todo.model';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-list',
-  standalone: true,
-  imports: [],
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.css'
+  styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  todos: Todo[] = [];
 
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit(): void {
+    this.todos = this.todoService.getTodos();
+  }
+
+  deleteTodo(id: number): void {
+    this.todoService.deleteTodo(id);
+    this.todos = this.todoService.getTodos();
+  }
 }
